23685c30826acd8f76a9400a9eb46bf18095b5ad
SVingelsgaard <sigurd@vingelsgaard.no>
2022-01-29 23:33:32 +0100

    x

---
+++ main.py
@@ -0,0 +1,70 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang.builder import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen, NoTransition
+from kivy.clock import Clock
+from kivy.config import Config
+from kivy.uix.image import Image
+from kivy.uix.button import Button
+from kivy.properties import NumericProperty
+from kivy.properties import ListProperty
+from kivy.uix.scatterlayout import ScatterLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
+from kivy.core.text import Label
+import math
+import matplotlib.pyplot as plt
+from kivy.graphics import Line
+import time
+
+#window settings
+Config.set('graphics', 'window_state', 'maximized')
+Config.set('graphics', 'borderless', '0')
+Config.set('graphics', 'fullscreen', '1')
+#Config.set('graphics',
+
+
+
+class WindowManager(ScreenManager):
+    pass
+
+class StartScreen(Screen):
+    pass
+
+class MainScreen(Screen):
+    pass
+
+#load kv file
+kv = Builder.load_file("frontend/main.kv")
+
+class GUI(App):
+    
+    def on_start(self): #variables
+        #system variables
+        self.setCYCLETIME = 0.02
+        self.runTime = 0
+
+        #program variables
+
+    #continus cycle
+    def cycle(self, readCYCLETIME):
+        if self.runTime != 0 and self.runTime < .03:
+            time.sleep(1)
+        
+        self.runTime += readCYCLETIME
+
+    #runns cycle
+    def runApp(self):
+        Clock.schedule_interval(self.cycle, self.setCYCLETIME)
+
+
+    #runs myApp(graphics)
+    def build(self):
+        return kv
+
+#runs program and cycle
+if __name__ == '__main__':
+    GUI().run()
+
+
